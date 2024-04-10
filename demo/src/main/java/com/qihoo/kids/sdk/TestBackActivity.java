8770aeb9f27da2de2959908f3c6ad3202435bc65
wangxuejie <wangxuejie@360.cn>
2024-04-10 15:36:18 +0800

    Update the method invocation method for setAllowAppIfBackground

--- demo/src/main/java/com/qihoo/kids/sdk/TestBackActivity.java
+++ demo/src/main/java/com/qihoo/kids/sdk/TestBackActivity.java
@@ -1,12 +1,18 @@
 package com.qihoo.kids.sdk;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.URLDecoder;
 
 import qihoo.sdk.QWatch;
 
@@ -25,8 +31,7 @@ public class TestBackActivity extends FragmentActivity {
 
         //1. 初始化 QWatch SDK
 //        QWatch.init(this); TestApp中已经调用
-        //2. 申请后台免杀权
-        QWatch.setAllowAppIfBackground(true);
+
 
         initView();
     }
@@ -36,17 +41,20 @@ public class TestBackActivity extends FragmentActivity {
         tvObtainBgWhitelistPermissions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //2. 申请后台免杀权
+                QWatch.setAllowAppIfBackground(true);
                 startPickImageActivity();
             }
         });
     }
 
+    private final int REQUEST_CODE_PICK_IMAGE = 1000;
+    private final String ACTION_PICK_PHOTO = "com.qihoo.kids.gallery.ACTION_PICK";
+    private final String KEY_PICK_FROM_CAMERA_ENABLE = "PICK_FROM_CAMERA_ENABLE";
+    private final String KEY_EXTRA_ACTION_TYPE = "key_extra_action_type";
+    private final int OK = 0;
+
     private void startPickImageActivity() {
-        String ACTION_PICK_PHOTO = "com.qihoo.kids.gallery.ACTION_PICK";
-        String KEY_PICK_FROM_CAMERA_ENABLE = "PICK_FROM_CAMERA_ENABLE";
-        int REQUEST_CODE_PICK_IMAGE = 1000;
-        String KEY_EXTRA_ACTION_TYPE = "key_extra_action_type";
-        int OK = 0;
         try {
             Intent intent = new Intent(ACTION_PICK_PHOTO);
             intent.setType("image/*");
@@ -60,10 +68,35 @@ public class TestBackActivity extends FragmentActivity {
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         //3. 释放后台免杀权
         QWatch.setAllowAppIfBackground(false);
+        if (requestCode == REQUEST_CODE_PICK_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    if (data == null) {
+                        return;
+                    }
+                    Uri uri;
+                    if (data.hasExtra(MediaStore.EXTRA_OUTPUT)) {
+                        uri = data.getParcelableExtra(MediaStore.EXTRA_OUTPUT);
+                    } else {
+                        uri = data.getData();
+                    }
+                    if (uri != null) {
+                        String path = URLDecoder.decode(uri.getAuthority() + uri.getPath(), "UTF-8");
+                        if (!TextUtils.isEmpty(path)) {
+                            Log.i(TAG, "onActivityResult: image path : " + path);
+                            Toast.makeText(this, "path=" + path, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "onActivityResult: e= " + e.getMessage());
+                }
+            }
+        }
     }
 
 }
